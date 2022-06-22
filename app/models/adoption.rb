ba999cede3702afd17f022aa9c2ac01d04d6b271
mathia pagani <mathiapagani@gmail.com>
2022-06-22 14:05:41 +0200

    validations ok

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -2,4 +2,6 @@ class Adoption < ApplicationRecord
   belongs_to :user
   belongs_to :tree
   has_one :review, dependent: :destroy
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :adoption
   belongs_to :user
+  validates :rating, presence: true
+  validates :rating, length: { in: 0..5 }
+  validates :content, length: { in: 3..114 }
 end
